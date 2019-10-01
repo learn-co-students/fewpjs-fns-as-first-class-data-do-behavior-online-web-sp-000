d756ed84bd29717af3cb71470acab179694ffb90
Andrew Davisson <adavisson@Andrews-MacBook-Air.local>
2019-10-01 15:07:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStamp) {
+  let times = timeStamp.split(":");
+  if (times[0] < 12) {
+    return "Good Morning";
+  } else if (times[0] > 11 && times[0] < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const node = document.getElementById("greeting");
+  node.innerText = message;
+}
