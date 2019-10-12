0668a3baff3b2acc87fcf0ed43e6b16f84b37f99
Jane Venter <janeanneventer@gmail.com>
2019-10-12 15:49:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-/* Given Code, don't edit */
+/* Given Code, don't edit*/
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
+
+function greet(timeS) {
+  let time = timeS.split('');
+  
+  time.splice(2, 1);
+  let ti = time.join('');
+  let newtime = parseInt(ti, 10);
+  let greeting;
+  if (newtime < 1200) {
+    greeting = "Good Morning";
+  } else if (newtime > 1200 & newtime < 1700){
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+  return greeting;
+}
+
+function displayMessage(string){
+  let g = document.getElementById("greeting");
+  g.innerText = string;
+  
+} 
 
 /* End Given Code, don't edit above here...*/
 
