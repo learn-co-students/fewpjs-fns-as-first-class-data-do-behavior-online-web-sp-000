478bd99e1dd3f4136fb229a91cd0d92c4dc1f41e
Adil Naimi <adilnm23@hotmail.com>
2019-11-10 21:58:00 -0600

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(num) {
+  let hr=parseInt(num.split(':')[0]);
+  let min=parseInt(num.split(':')[1]);
+  if (hr<12)
+    return "Good Morning";
+  else if (hr>=12 && hr<=17) {
+    return "Good Afternoon";
+  }
+  else if (hr>17) {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(func) {
+  document.getElementById("greeting").innerText=func;
+}
