f16f22438b72b244b93aeea7cd6713bb9e972410
Lindsey Wells <github email address>
2020-01-03 20:35:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
+function greet(timeString) {
+    const splitTime = timeString.split(':');
+    const hour = parseInt(splitTime[0], 10);
+
+    if (hour > 17) return "Good Evening"
+    if (hour > 12) return "Good Afternoon"
+    return "Good Morning"
+}
+
+
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+    let greetingNode = document.getElementById('greeting')
+    greetingNode.innerText = string
+
+}
