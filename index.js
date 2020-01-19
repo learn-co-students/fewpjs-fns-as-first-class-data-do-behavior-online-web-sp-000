72b9fa252d6016fd1f4ca332fc2f79fbd7c463fb
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 04:01:00 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,22 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(input) {
+  let wordArray = input.split(':');
+  let num = parseInt(wordArray[0]);
+  let greeting = null;
+  if (num < 12) {
+    greeting = 'Good Morning';
+  }
+  else if (num >= 12 && num <= 17) {
+    greeting = 'Good Afternoon';
+  }
+  else {
+    greeting = 'Good Evening';
+  }
+  return greeting;
+}
+
+function displayMessage(input) {
+  document.getElementById("greeting").innerHTML = "${input}";
+}
