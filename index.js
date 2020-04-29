9ed57f0c907e926625aea05b7a6f4b5f0f068141
Christopher Dent <christopherdent01@gmail.com>
2020-04-28 21:33:06 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
+
+
+function greet(time) {
+  const hour = parseInt(time);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  if ( hour <17 && hour > 12 ) return "Good Afternoon"
+  }
