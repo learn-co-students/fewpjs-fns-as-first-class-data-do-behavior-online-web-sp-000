002624a9f98268fda712ac363560e51b40bc62f9
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-17 00:30:57 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const time = timeString.split(' ');
+  if (parseInt(time[0]) < 12) {
+    return 'Good Morning';
+  }else if (parseInt(time[0]) > 12 && parseInt(time[0]) < 17 ) {
+      return 'Good Afternoon';
+    }else {
+      return 'Good Evening'
+    }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(oneString) {
+  document.querySelector("#greeting").innerText = oneString
+  return
+}
