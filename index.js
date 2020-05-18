db89c482891045519aa1b0beb4b9bfa09522ccaa
andrewsilva9 <andrewsilva9@gmail.com>
2020-05-18 11:15:11 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timestamp) {
+  let times = timestamp.split(':');
+  if (times[0] < 12) {
+    return 'Good Morning';
+  } else if (times[0] > 12 && times[0] < 17) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const node = document.getElementById("greeting");
+  node.innerText = message;
+}
