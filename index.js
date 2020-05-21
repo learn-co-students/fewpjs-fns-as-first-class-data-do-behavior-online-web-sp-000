55ea4ce80377e5525091477feaed17c808c08d7c
rehmanali001 <rehmanali001@gmail.com>
2020-05-21 14:15:58 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(input) {
+  const time = parseInt(input)
+  if (time < 12) {
+    return `Good Morning`;
+  } else if (time >= 12 && time <= 17) {
+    return `Good Afternoon`;
+  } else if (time > 17) {
+    return `Good Evening`;
+  }
+}
+
+
+function displayMessage(testContent) {
+  let content = document.getElementById("greeting");
+  content.innerText = testContent
+}
