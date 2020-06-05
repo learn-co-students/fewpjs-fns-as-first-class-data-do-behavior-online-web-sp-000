2651a2f70088abdf4b23c1bf6cf5e8e1d552a8f1
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-06-04 17:39:18 -0700

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const hour = parseInt(timeString.split(":")[0]);
+  if (hour >= 0 && hour < 12) {
+    return "Good Morning";
+  } else if  (hour >= 12 && hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(greeting) {
+  const messageNode = document.querySelector("h1#greeting");
+  messageNode.innerText = greeting;
+}
