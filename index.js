d084b5ba085e2ce4d3032159ff17b6bfb06aab9b
SPerfors <stevecanbuild@gmail.com>
2020-07-08 17:05:54 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+let time = '';
+
+function greet() {
+  if (time < 1200 ) {
+    console.log('Good Morning')
+  } else if (time <= 1700 ) {
+    console.log('Good Afternoon')
+  } else {
+    console.log('Good Evening')
+  }
+}
 /* Write your implementation of displayMessage() */
