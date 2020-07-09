d4e2bb84b97f14c80f2dce5cb764f88967177532
SPerfors <stevecanbuild@gmail.com>
2020-07-09 06:41:13 -0600

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-let time = '';
 
-function greet() {
-  if (time < 1200 ) {
-    console.log('Good Morning')
-  } else if (time <= 1700 ) {
-    console.log('Good Afternoon')
+function greet(string) {
+  if (string.split(':')[0] > 17 ) {
+    return 'Good Evening';
+  } else if (string.split(':')[0] < 17 && string.split(':')[0] >= 12) {
+    return 'Good Afternoon';
   } else {
-    console.log('Good Evening')
+    return 'Good Morning';
   }
 }
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.querySelector('#greeting').innerText = string;
+}
