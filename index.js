935a2565dc380bf0cde764b187e97696c9e8477b
ramseychapin <ramsey@ramseychapin.com>
2020-08-22 12:55:05 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+	const givenHour = parseInt(time, 10);
+
+	if (givenHour < 12){
+		return "Good Morning"	
+	} else if (givenHour > 17) {
+		return "Good Evening"
+	} else {
+		return "Good Afternoon"
+	};
+}
+
+function displayMessage(message) {
+	document.getElementById("greeting").innerText = message;
+}
