bfe23117b9b4c9c3ca8d5d7aba52c6306916661f
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-27 16:25:05 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  let hour = string.split(':')[0];
+  let parsed = parseInt(hour);
+  if (parsed < 12)  {
+    return "Good Morning";
+  }
+  if (parsed < 17)  {
+    return "Good Afternoon";
+  }
+  return "Good Evening";
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
