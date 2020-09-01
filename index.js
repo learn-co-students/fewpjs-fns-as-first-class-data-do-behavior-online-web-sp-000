8e754f2c48cc91bcd999eb3aa6607943a6e5e314
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-09-01 06:56:46 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+	let hour = time.split(":")[0]
+	if (hour < 12) {
+		return "Good Morning"
+	} else if (hour < 17) {
+		return "Good Afternoon"
+	} else {
+		return "Good Evening"
+	}
+}
+
+function displayMessage(message) {
+	document.querySelector("h1#greeting").innerText = message;
+}
