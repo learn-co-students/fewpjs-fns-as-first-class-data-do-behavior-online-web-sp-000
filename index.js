c8d42129b2f240c7ca5a6e66f186b8b7a2f9c1cd
ting682 <tchung682@gmail.com>
2020-09-08 13:14:44 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  
+  let timeSplit = timeString.split(":")
+  let timeHour = timeSplit[0]
+  let timeMinute  = timeSplit[1]
+
+  if (timeHour < 12) {
+    return "Good Morning"
+
+  } else if (timeHour > 12 && timeHour < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  let greeting = document.getElementById("greeting")
+  greeting.innerText = message
+}
+
+done()
