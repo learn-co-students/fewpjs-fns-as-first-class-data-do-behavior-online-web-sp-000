369e9fe88ca974871272479ab193504294980a86
Jivefox <https://github.com/Jivefox>
2020-10-18 14:41:13 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(timeString) {
+  let num = timeString.split("00")[0];
+  let time = parseInt(num, 10);
+  if (time < 12) return "Good Morning";
+  if (time > 17) return "Good Evening";
+  return "Good Afternoon"
+}
 
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
