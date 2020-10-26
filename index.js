b675d23f02c8317e08fe39f0b05f86395b3848ea
Yumacorn <ReachRyanMui@gmail.com>
2020-10-25 22:53:23 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet() {
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  
+}
