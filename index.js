6e6811bb17b75aea56baa74a8523c772430ed21c
satodd1967 <seth.todd@outlook.com>
2020-11-10 20:44:03 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time) {
+  let tod = parseInt(time.split(":")[0] + time.split(":")[1]);
 
+  if (tod < 1200) {
+    return "Good Morning";
+  }
+
+  else if (tod > 1700) {
+    return "Good Evening";  
+  }
+
+  else {
+    return "Good Afternoon";
+  }
+}
 /* Write your implementation of greet() */
+function displayMessage(greeting) {
+  let words = document.getElementById("greeting")
+  words.innerText = greeting
+
+}
+
+
+
 /* Write your implementation of displayMessage() */
