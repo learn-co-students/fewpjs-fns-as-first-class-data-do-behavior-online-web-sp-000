4942c56f3f374c7ad5b929cf94690371a8dce91a
Gearz <shanegeary@yahoo.com>
2020-11-28 17:17:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* Given Code, don't edit */
+const { Test } = require("element");
 
 document.querySelector("button").addEventListener("click", handleClick);
 
@@ -10,4 +11,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let time = timeString.slice(0,2) + timeString.slice(3);
+  time = parseInt(time, 10);
+  if (time < 1200) {
+    return 'Good Morning';
+  } else if ((time >= 1200) && (time <= 1700)) {
+    return 'Good Afternoon';
+  } else if (time > 1700 ) {
+    return 'Good Evening';
+  } else {
+    return 'TEST';
+  }; 
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(timeString) {
+  let hello = document.getElementById("greeting");
+  hello.innerText = greet(timeString);
+
+}
