8643b04fae47f7774841c39f69adea45286c0c4a
Mike Barberry <mikebarberry@protonmail.com>
2020-11-30 23:26:54 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet (hour) {
+  const time = parseInt(hour, 10);
+  if (time < 12) {
+    return "Good Morning"
+  } else if (time > 12 && time < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+console.log(greet(15))
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
