f7cb5e5a4d8bdfd3bcaa5248f0bceb82ff1d4255
Jasmine Carter <jasmine.carter87@gmail.com>
2020-11-30 23:54:43 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(time) {
+if (parseInt(time) < 12) {
+  return "Good Morning";
+}
+if (parseInt(time) >= 12 && parseInt(time) < 17) {
+  return "Good Afternoon"
+}
+return "Good Evening"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+ document.getElementById("greeting").innerText = message;
+
+}
