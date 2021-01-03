49ddece1ce69bae98607d86e340f37d7e48aff71
Drew Thompson <drewallynthompson@gmail.com>
2021-01-03 07:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,16 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(string) {
+  let number = parseInt((string.split(":"))[0]);
+  if (number < 12) {
+    return "Good Morning";
+  } else if ( number > 12 && number < 17) {
+    return "Good Afternoon"
+  } else if ( number > 17) {
+    return "Good Evening"
+  };
+};
+function displayMessage(string){
+  document.getElementById('greeting').innerText = string
+};
