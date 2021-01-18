89aa4ba30100248fcfa533869b663f1e97dcbbee
William Pannell <lpann5@gmail.com>
2021-01-17 19:04:23 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
 /* Write your implementation of displayMessage() */
+
+function greet(timeVar){
+  if ( parseInt(timeVar, 10) < 12 ) return "Good Morning"
+  if ( parseInt(timeVar, 10) > 17 ) return "Good Evening"
+  return "Good Afternoon"
+}
