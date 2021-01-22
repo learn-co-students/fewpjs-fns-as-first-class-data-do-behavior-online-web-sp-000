33b1115daabcc9f37b489a7f123efc40f590c3b5
TrueC-D <aletheacary@gmail.com>
2021-01-21 21:36:12 -0800

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString){
+  const time = timeString.split(':');
+  const hh = parseInt((time[0]), 10);
+  const mm = parseInt((time[1]), 10);
+  if (hh == NaN || mm == NaN) {
+    return 'Invalid Time, must be in HH:MM 24 hour format'
+  };
+  if (hh <12) {
+    return "Good Morning"
+  };
+  if (hh >12 && hh <17) {
+    return "Good Afternoon"
+  };
+  return "Good Evening"
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  document.getElementById('greeting').innerText = message}
