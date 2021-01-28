ab7fb48d6bb53e85c157b68e6bae60c3f2b82bd6
anpham148 <anpham148@gmail.com>
2021-01-28 09:02:26 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let greeting;
+  let hour = parseInt(timeString, 10);
+  if (hour < 12)  {
+    greeting = 'Good Morning';
+  } else if (hour > 17) {
+    greeting = 'Good Evening';
+  } else if (hour > 12 && hour < 17) {
+    greeting = 'Good Afternoon';
+  } else {
+    greeting = 'Please insert a string which specifies the 24-hour time in the format HH:MM';
+  }
+   return greeting
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
