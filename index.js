312b4a239a63f5f44fc7cc74cd77c65be443adbc
Catherine Bronzo <cbronzo@mac.com>
2021-02-07 03:26:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+	const hour = timeString.split(':');
+	const time = parseInt(hour);
+	if (time < 12) {
+		return "Good Morning";
+	} else if (time < 17) {
+		return "Good Afternoon";
+	} else {
+		return "Good Evening";
+	}
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+	document.getElementById('greeting').innerText = msg;
+}
