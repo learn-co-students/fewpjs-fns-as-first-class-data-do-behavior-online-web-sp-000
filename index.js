b3e6e0291fb17d42d491c771b8a6c5c15ccd8d9c
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-18 14:36:39 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString) {
+  const splitString = timeString.split(':')[0];
+  const timeInt = parseInt(splitString, 10)
+  if (timeInt < 12) {
+    return "Good Morning"
+  } else if (timeInt < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+  
+};
+
+function displayMessage(message) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = message
+}
