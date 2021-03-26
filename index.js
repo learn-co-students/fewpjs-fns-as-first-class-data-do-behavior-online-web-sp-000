f134c5d35602bc2d4feb60d7a43daff2df24b26b
dmepilot <dmepilot@yahoo.com>
2021-03-26 10:31:49 -0600

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+let nums = time.split(":");
+// let num = nums.join();
+let numm = parseInt(nums[0], 10);
+
+if (numm < 12) {
+  greeting = "Good Morning";
+}
+else if (numm >= 12 && numm <=17) {
+  greeting = "Good Afternoon";
+}
+else if (numm > 17) {
+  greeting = "Good Evening";
+}
+ return greeting;
+}
+
+
+function displayMessage(message) {
+  let element = document.querySelector("h1#greeting");
+  element.innerText = message;
+
+}
