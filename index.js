75827cf24564d008fc5df1df2a581c7528ac9be4
abduamal <abduamal@ymail.com>
2021-04-12 01:32:19 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+
+  /* Takes one argument*/
+function greet(str){
+  /* converts the string from <input> into the 24 hour format*/
+  let hour = str.split(':')[0];
+  if (hour < 12) return "Good Morning";
+  if (hour > 17) return "Good Evening";
+  return "Good Afternoon";
+}
 /* Write your implementation of displayMessage() */
+  /* Takes one argument */
+function displayMessage(str){
+  /* Updates the text inside the #greeting node with the content of the argument*/
+  document.getElementById('greeting').innerText = str;
+  /* Does not return anything*/
+}
+
