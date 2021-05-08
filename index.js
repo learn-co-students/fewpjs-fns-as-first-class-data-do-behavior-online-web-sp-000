a842ad6d761f63ae7b10eb413cdc885c9843335a
bongofury <jtroutmanman@gmail.com>
2021-05-08 17:56:17 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  time = time.split(":");
+  time = parseInt(time);
+  
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time > 12 && time < 17) {
+    return "Good Afternoon";
+  }
+    else {
+      return "Good Evening";
+  }
+}
+
+function displayMessage(string) {
+  let greeting = document.getElementById('greeting');
+  greeting.innerHTML = string;
+}
