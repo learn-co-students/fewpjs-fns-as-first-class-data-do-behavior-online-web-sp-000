508a4d250bcb35df5d593ab5efc3e22ba730a58e
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-19 00:39:14 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  let strings = string.split(":");
+  let hours = strings[0];
+  let minutes = strings[1];
+  if (hours < 12){
+    return "Good Morning";
+  } else if (hours >= 12 && hours < 16){
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+  
+  console.log(string);
+  
+  let location = document.getElementById('greeting');
+  location.textContent = string;
+}
