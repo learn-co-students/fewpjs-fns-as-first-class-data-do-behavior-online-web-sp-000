d3c07ff1bc48638d64272cfcf3c89c23e92d2d5b
russell-macalintal <russell-macalintal@gmail.com>
2021-05-21 03:05:34 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const timeDigits = timeString.split(':');
+  const timeNumber = parseInt(timeDigits[0]) * 100 + parseInt(timeDigits[1]);
+  
+  if (timeNumber >= 0 && timeNumber < 1200) {
+    return 'Good Morning'
+  } else if (timeNumber >= 1200 && timeNumber <= 1700) {
+    return 'Good Afternoon'
+  } else {
+    return 'Good Evening'
+  } 
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
